e6e75667a9b7ecbaf16d12704cb523c265b64420
Luis Samano <martindjt@hotmail.com>
2019-03-31 12:47:02 -0600

    init

---
+++ models.py
@@ -0,0 +1,46 @@
+# models.py
+
+from flask_login import UserMixin
+from . import db
+
+
+class AreaModel(db.Model):
+    __tablename__ = 'area'
+
+    id = db.Column(db.Integer, primary_key=True, autoincrement=True)
+    area = db.Column(db.String(60), unique=True, nullable=False)
+
+
+class PuestoModel(db.Model):
+    __tablename__ = 'puesto'
+
+    id = db.Column(db.Integer, primary_key=True, autoincrement=True)
+    area = db.Column(db.Integer, db.ForeignKey(AreaModel.id), nullable=False)
+    descripcion = db.Column(db.String(200), nullable=True)
+
+
+class RoleModel(db.Model):
+    __tablename__ = 'puesto'
+
+    id = db.Column(db.Integer, primary_key=True, autoincrement=True)
+    role = db.Column(db.Integer, unique=True)
+
+    
+class UsuarioModel(UserMixin, db.Model):
+    __tablename__ = 'usuario'
+    # primary keys are required by SQLAlchemy
+    id = db.Column(db.Integer, primary_key=True)
+    email = db.Column(db.String(100), unique=True)
+    password = db.Column(db.String(100))
+    name = db.Column(db.String(1000))
+
+
+class PublicacionModel(db.Model):
+    __tablename__ = 'publicacion'
+
+    id = db.Column(db.Integer, primary_key=True, autoincrement=True)
+    iduser = db.Column(db.String(120), db.ForeignKey(
+        UsuarioModel.id), nullable=False)
+    asunto = db.Column(db.String(100), nullable=False)
+    descripcion = db.Column(db.String(200), nullable=True)
+    documento = db.Column(db.String(200), nullable=False)
